941d84219ad2466198ba5a86ce51335bc75b3664
anshojha <anshojha24@gmail.com>
2022-12-19 23:46:07 +0530

    Promise

--- promise.js
+++ promise.js
@@ -1,16 +1,47 @@
-let p = new Promise((resolve, reject ) => {
- let a =1 +1
- if(a==3){
-    resolve('Success')
- }
- else{
-    reject('Failed')
- }
-})
+// let p = new Promise((resolve, reject ) => {
+//  let a =1 +1
+//  if(a==3){
+//     resolve('Success')
+//  }
+//  else{
+//     reject('Failed')
+//  }
+// })
 
 
-p.then((message) => {
- console.log('This is in the then ' + message)
-}).catch((message)=>{
-    console.log("This is in the catch "+ message)
-})
+// p.then((message) => {
+//  console.log('This is in the then ' + message)
+// }).catch((message)=>{
+//     console.log("This is in the catch "+ message)
+// })
+
+
+//******** Use of Arrow function to implement*******
+
+
+// const userLeft = true
+// const userWatcingCatMeme =true
+
+// function watchTutorialCallbak(callback, errorCallback) {
+//    if (userLeft) {
+//       errorCallback({
+//          name: 'User Left',
+//          message: '('
+//       })
+//    }
+//    else if (userWatcingCatMeme) {
+//       errorCallback({
+//        name : 'User Watching Cat meme',
+//        message : 'Message <3'
+//       })
+//    }
+//    else{
+//       callback('Thumbs up and subscribe!!')
+//    }
+// }
+
+// watchTutorialCallbak((message)=>{
+//    console.log('success :'+ message);
+// }, (error) =>{
+//    console.log(error.name + ' ' + error.message)
+// })
