242cd8e1252cb28986cdb11a805fc66bae9fbbe8
anshojha <anshojha24@gmail.com>
2023-01-12 23:57:33 +0530

    Reduce method

---
+++ ArrayTopic/reduce.js
@@ -0,0 +1,21 @@
+const items  = [
+    {name : 'Bike' , price : 10},
+    {name : 'TV' , price : 100},
+    {name : 'Album' , price : 130},
+    {name : 'Book' , price : 10},
+    {name : 'Phone', price : 130},
+    {name : 'Computer', price : 10},
+    {name : 'Keyboard' , price : 300},
+]
+
+ items.forEach((item)=>{
+    return console.log(item );
+})
+ items.forEach((item)=>{
+    return console.log(item.price );
+})
+
+ items.forEach((item)=>{
+    return console.log(item.name );
+})
+
