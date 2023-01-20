1fcfffdcb793aa300beb1ce9d0cf2abf444d265a
anshojha <anshojha24@gmail.com>
2023-01-21 00:54:20 +0530

    DOM Practise

---
+++ DOM_Practise/script.js
@@ -0,0 +1,14 @@
+// let ctitle = document.getElementById("firstChile");
+
+// ctitle.style.color = "Yellow";
+
+
+let ctitles = document.querySelectorAll(".card-title");
+ctitles[0].style.color = "Red";
+ctitles[1].style.color = "Green";
+ctitles[2].style.color = "Pink";
+console.log(ctitles);
+
+
+document.querySelector(".this").style.color="Black";
+document.querySelector(".this").style.background="Yellow";
