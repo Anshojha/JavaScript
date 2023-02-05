fdacd89de4762679e6d91155b9af248e16e3737a
anshojha <anshojha24@gmail.com>
2023-02-05 20:46:37 +0530

    Fetching Api using Js

--- Snake Water Gun/snake.js
+++ Snake Water Gun/snake.js
@@ -1,18 +1,26 @@
 let user = prompt("Enter S , G and W");
 
-let cpu1 = Math.floor(Math.random()*3);
+let cpu1 = Math.floor(Math.random() * 3);
 
-let cpu = ["S","W","G"][cpu1];
+let cpu = ["S", "W", "G"][cpu1];
 
-let CpuScore = 0, userScore =0;
+//let CpuScore = 0, userScore = 0;
 
-
-const match  = (user , cpu)=>{
-    if(cpu===user){
+const match = (user, cpu) => {
+    if (cpu === user){
         return 0;
     }
-    else if(cpu === "S" && user ==="W"){
-        userScore++;
+    else if (cpu === "S" && user === "W") {
+        return "Snake";
     }
-    
+    else if (cpu === "W" && user === "S") {
+        return ""
+    }
+    else if (cup === "G" && user === "S") {
+   
+    }
+    else if (cup === "S" && user === "G") {
+      
+    }
+
 }
